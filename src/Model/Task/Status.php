aedfe4f363548a7dd89a053a9b067f6f86487ac8
Wouter Samyn <wouter.samyn@studioemma.com>
2020-11-11 11:16:16 +0100

    Add a listing to fetch maitenance tasks
    This will return a collection of models holding:
    - the name of the task
    - if the job is locked (based on key)

---
+++ src/Model/Task/Status.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace MaintenanceToolboxBundle\Model\Task;
+
+use Symfony\Component\Lock\Key;
+
+class Status
+{
+    /** @var string */
+    private $task;
+    /** @var bool */
+    private $locked = false;
+    /** @var \DateTimeImmutable */
+    private $expirationDate;
+
+    /**
+     * Status constructor.
+     *
+     * @param string $task Name of the maintenance task
+     */
+    public function __construct(string $task)
+    {
+        if (empty($task)) {
+            throw new \InvalidArgumentException(
+                'No task name was given for the job'
+            );
+        }
+
+        $this->task = $task;
+    }
+
+    /**
+     * Generate a Status based on the taskname
+     *
+     * @param string $task
+     * @return static
+     */
+    public static function fromTask(string $task): self
+    {
+        return new self($task);
+    }
+
+    /**
+     * Return the name of the maintenance task
+     *
+     * @return string
+     */
+    public function getTask(): string
+    {
+        return $this->task;
+    }
+
+    /**
+     * Return the state of the lock
+     *
+     * @return bool
+     */
+    public function isLocked(): bool
+    {
+        return $this->locked;
+    }
+
+    /**
+     * Return the date when the lock will be released
+     *
+     * @return \DateTimeImmutable
+     */
+    public function getExpirationDate(): \DateTimeImmutable
+    {
+        return $this->expirationDate;
+    }
+
+    /**
+     * @param bool $locked
+     * @return Status
+     */
+    public function setLocked(bool $locked): self
+    {
+        $this->locked = $locked;
+        return $this;
+    }
+
+    /**
+     * @param \DateTimeImmutable $expirationDate
+     * @return Status
+     */
+    public function setExpirationDate(\DateTimeImmutable $expirationDate): self
+    {
+        $this->expirationDate = $expirationDate;
+        return $this;
+    }
+
+    /**
+     * Return the number of seconds the job was locked
+     *
+     * @return int
+     */
+    public function getDurationSeconds(): int
+    {
+        // By default the locks are set for 24 hours,
+        // so the start time will be 1 day before the expiration
+        $started = $this->getExpirationDate()->sub(new \DateInterval('P1D'));
+        return time() - $started->getTimestamp();
+    }
+
+    /**
+     * Human readable output of the time the job was locked
+     *
+     * @return string
+     */
+    public function getDurationString(): string
+    {
+        $duration = $this->getDurationSeconds();
+        return sprintf('%02dh%02dm%02ds', ($duration / 3600), ($duration / 60 % 60), $duration % 60);
+    }
+
+    /**
+     * Generate the according key for the maintenance job
+     *
+     * @return Key
+     */
+    public function getKey(): Key
+    {
+        return new Key('maintenance-' . $this->getTask());
+    }
+}
---
+++ src/Service/TaskListing.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace MaintenanceToolboxBundle\Service;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use MaintenanceToolboxBundle\Model\Task\Status;
+use Pimcore\Maintenance\Executor;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Lock\Factory as LockFactory;
+use Symfony\Component\Lock\PersistingStoreInterface;
+
+class TaskListing
+{
+    /** @var Executor */
+    private $maintenanceExecutor;
+    /** @var LoggerInterface */
+    private $logger;
+    /** @var LockFactory */
+    private $lockFactory;
+    /** @var PersistingStoreInterface */
+    private $store;
+
+    /**
+     * TastListing constructor.
+     *
+     * @param Executor $maintenanceExecutor
+     * @param LoggerInterface $logger
+     * @param LockFactory $lockFactory
+     * @param PersistingStoreInterface $store
+     */
+    public function __construct(
+        Executor $maintenanceExecutor,
+        LoggerInterface $logger,
+        LockFactory $lockFactory,
+        PersistingStoreInterface $store
+    ) {
+        $this->maintenanceExecutor = $maintenanceExecutor;
+        $this->logger = $logger;
+        $this->lockFactory = $lockFactory;
+        $this->store = $store;
+    }
+
+    /**
+     * Return the full list of tasks, including their locked status
+     *
+     * @return ArrayCollection
+     */
+    public function getTasks(): ArrayCollection
+    {
+        $tasks = new ArrayCollection();
+
+        foreach ($this->maintenanceExecutor->getTaskNames() as $taskName) {
+            $status = Status::fromTask($taskName);
+            // Create dummy lock of 0 seconds
+            $lock = $this->lockFactory->createLock($status->getKey(), 0);
+            // If the lock cannot be acquired, the job already is locked
+            $status->setLocked(!$lock->acquire());
+
+            // Add to the collection
+            $tasks->add($status);
+        }
+
+        return $tasks;
+    }
+}
