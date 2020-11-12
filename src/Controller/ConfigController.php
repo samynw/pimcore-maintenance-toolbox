46b1f117fc0eeef7283895dbb2132c9dbaa0bbde
Wouter Samyn <wouter.samyn@studioemma.com>
2020-11-12 22:16:12 +0100

    Add controller/view for bundle configuration
    Will be used as admin config form for Pimcore Bundle

---
+++ src/Controller/ConfigController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MaintenanceToolboxBundle\Controller;
+
+use MaintenanceToolboxBundle\Form\EditConfig;
+use MaintenanceToolboxBundle\Tool\ArrayFormatter;
+use MaintenanceToolboxBundle\Config\ToolboxConfig;
+use Pimcore\Bundle\AdminBundle\Controller\AdminController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConfigController extends AdminController
+{
+    /** @var EditConfig */
+    private $editForm;
+
+    /**
+     * ConfigController constructor.
+     *
+     * @param EditConfig $formService
+     */
+    public function __construct(EditConfig $formService)
+    {
+        $this->editForm = $formService;
+    }
+
+    /**
+     * Show the form to edit the configuration
+     * and update the config file upon saving
+     *
+     * @param Request $request
+     * @return Response
+     * @Route("/admin/maintenance-toolbox/config", name="maintenancetoolbox_config")
+     */
+    public function configAction(Request $request): Response
+    {
+        $config = new ToolboxConfig();
+
+        $form = $this->editForm->buildForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $formatter = new ArrayFormatter();
+            $config->save($formatter->toNestedArray($data));
+        }
+
+        return $this->render('@MaintenanceToolbox/config/config.html.twig',
+            [
+                'config' => $config,
+                'form' => $form->createView(),
+            ]
+        );
+    }
+}
--- src/Tool/ArrayFormatter.php
+++ src/Tool/ArrayFormatter.php
@@ -30,4 +30,52 @@ class ArrayFormatter
 
         return $result;
     }
+
+    /**
+     * Convert flat array with structured keys into multidimensional array
+     *
+     * from ['a__b__c' => 'foo' ]
+     * to ['a' => ['b' => ['c' => 'foo']]]
+     *
+     * @param array $input
+     * @return array
+     */
+    public function toNestedArray(array $input): array
+    {
+        $output = [];
+
+        foreach ($input as $key => $value) {
+            $this->setNestedOption($output, $key, $value);
+        }
+
+        return $output;
+    }
+
+    /**
+     * Set a nested option:
+     * - create new level on each __ in the key
+     *
+     * @param array $data
+     * @param $key
+     * @param $value
+     */
+    private function setNestedOption(array &$data, $key, $value): void
+    {
+        $keys = explode('__', $key);
+
+        // Keep the last key aside to process as last
+        $last_key = array_pop($keys);
+
+        // For each key segment, create a new level in the array
+        while ($segmentKey = array_shift($keys)) {
+            if (!array_key_exists($segmentKey, $data)) {
+                $data[$segmentKey] = [];
+            }
+
+            $data = &$data[$segmentKey];
+        }
+
+        // set the final key
+        $data[$last_key] = $value;
+    }
 }
