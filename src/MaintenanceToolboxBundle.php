e0dd822db46a04e9447b31895c93337491ff829b
Wouter Samyn <wouter.samyn@studioemma.com>
2020-12-10 21:20:49 +0100

    Use the composer package info for bundle version info
    Instead of defining the version and description, use the composer info

--- src/MaintenanceToolboxBundle.php
+++ src/MaintenanceToolboxBundle.php
@@ -3,9 +3,12 @@
 namespace MaintenanceToolboxBundle;
 
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
 
 class MaintenanceToolboxBundle extends AbstractPimcoreBundle
 {
+    use PackageVersionTrait;
+
     public function getInstaller()
     {
         return $this->container->get(Installer::class);
@@ -14,5 +17,13 @@ class MaintenanceToolboxBundle extends AbstractPimcoreBundle
     public function getAdminIframePath(): string
     {
         return '/admin/maintenance-toolbox/config';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getComposerPackageName(): string
+    {
+        return 'samynw/pimcore-maintenance-toolbox';
     }
 }
